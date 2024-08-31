f0f8f360993bd19978a789058aa9e9e19683b939
Ardimas <>
2024-08-31 09:19:25 +0700

    Migration - Role -> Roles

--- database/migrations/2024_08_31_020640_create_roles_table.php
+++ database/migrations/2024_08_31_020640_create_roles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('role', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('role');
+        Schema::dropIfExists('roles');
     }
 };
