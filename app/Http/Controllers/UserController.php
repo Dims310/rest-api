94f4939de0be6aa850afef3348ad51b78556af0c
Ardimas <>
2024-08-31 09:15:27 +0700

    UserController - get all user request

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+
+    public function getUsers() {
+        return response()->json([
+            'error' => false,
+            'data' => [
+                User::get()
+            ]
+        ], 200);
+    }
+}
