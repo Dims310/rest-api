b50c2890966493f278f9367725c49341d62c6c1b
Ardimas <>
2024-08-31 09:18:31 +0700

    app.php - now support for CheckAdminMiddleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckAdminMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,7 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
             '/*',
         ]);
         $middleware->alias([
-            'auth.jwt' => \App\Http\Middleware\JWTMiddleware::class
+            'auth.jwt' => \App\Http\Middleware\JWTMiddleware::class,
+            'auth.admin' => CheckAdminMiddleware::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
