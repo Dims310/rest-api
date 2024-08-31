5609f59f6b00018a12099e791a2a59e28d619421
Ardimas <>
2024-08-31 09:18:14 +0700

    web.php - now support for CheckAdminMiddleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,6 +12,10 @@ Route::get('/csrftoken', function() {return csrf_token(); });
 
 Route::post('/user/register', [AuthController::class, 'register'])->name('register');
 Route::post('/user/login', [AuthController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth.jwt', 'auth.admin']], function() {
+    Route::get('/user', [UserController::class, 'getUsers'])->name('getUsers');
+});
 
 Route::middleware('auth.jwt')->get('/user/myprofile', [AuthController::class, 'myprofile']);
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
