aac23ae3322742b28bf69ae9b837b9995d7f2529
Ardimas <>
2024-09-07 09:52:33 +0700

    add ServiceModel, ServicePricesModel, TransactionModel

---
+++ app/Models/Transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'uuid',
+        'total_amount',
+        'status',
+        'user_id',
+        'service_id',
+        'created_at',
+        'updated_at'
+    ];
+}
