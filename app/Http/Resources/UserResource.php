43b9938c2649edff13ee9986e2c0dc4f3c2310bc
Ardimas <>
2024-09-07 08:58:35 +0700

    UserResource - add column for nim & phone

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,8 +15,11 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
+            'nim' => $this->nim,
             'name' => $this->name,
             'email' => $this->email,
+            'phone' => $this->phone,
             'role' => $this->role->name
         ];
     }
