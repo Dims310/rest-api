54eed52a894d8c4a31fd1aeb1329140472fdb1e5
Ardimas <>
2024-10-04 07:07:52 +0700

    Routes - new path

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/csrftoken', function() {return csrf_token(); });
 
@@ -27,3 +27,5 @@ Route::group(['middleware' => ['auth.jwt']], function() {
 
     Route::get('/transaction', [TransactionController::class, 'getAllTransactions'])->name('getAllTransactions');
 });
+
+Route::get('/service', [ServiceController::class, 'getServices'])->name('getServices');
