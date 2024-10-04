6fe82e8ed66bd4af2968579141e765ff62de1ad7
Ardimas <>
2024-10-04 07:00:38 +0700

    TransactionController - fix bad syntax

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -10,7 +10,18 @@ class TransactionController extends Controller
 {
     public function getAllTransactions() {
         $user = auth('api')->user();
-        $transaction = Transaction::where('user_id', $user->uuid)->get();
+        // $transaction = Transaction::where('user_id', $user->uuid)->get();
+        $transaction = Transaction::with('service')->where('user_id', $user->uuid)->get();
+
+        $transactionData = $transaction->map(function ($transaction) {
+            return [
+                'uuid' => $transaction->uuid,
+                'total_amount' => $transaction->total_amount,
+                'created_at' => $transaction->created_at,
+                'updated_at' => $transaction->updated_at,
+                'services' => $transaction->service->name
+            ];
+        });
 
         if ($user->role->name === "admin") {
             $transaction = Transaction::all();
@@ -20,7 +31,7 @@ class TransactionController extends Controller
             'error' => false,
             'data' => [
                 'message' => "Success get all transaction data",
-                'transaction' => $transaction
+                'transaction' => $transactionData
             ]
         ]);
     }
